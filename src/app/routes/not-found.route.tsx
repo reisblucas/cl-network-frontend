1a6a27b1e81bcf284d3a554757779bb241f3d8e0
reisblucas <lucasb.reis@hotmail.com>
2026-02-15 04:32:32 -0300

    feat: disable some nav from Navuse and add initial landing page

--- src/app/routes/not-found.route.tsx
+++ src/app/routes/not-found.route.tsx
@@ -6,7 +6,7 @@ export function NotFoundRoute() {
     <div className="pt-52 flex flex-col items-center font-semibold">
       <h1 className="text-4xl"> 404 - Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
-      <Link className="text-blue-300" to={paths.home.getHref()} replace>
+      <Link className="text-blue-300" to={paths.app.root.getHref()} replace>
         Go to home
       </Link>
     </div>
